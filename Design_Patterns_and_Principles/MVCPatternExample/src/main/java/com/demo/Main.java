17565030e25d1e1f00a83443058dac0acc21e12d
Sai Charan Parimi <pscharan2207@gmail.com>
2026-06-24 14:12:15 +0530

    MVC Pattern Completed

---
+++ Design_Patterns_and_Principles/MVCPatternExample/src/main/java/com/demo/Main.java
@@ -0,0 +1,32 @@
+package com.demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Create Model
+        Student student = new Student(
+                "Magnus",
+                101,
+                "A"
+        );
+
+        // Create View
+        StudentView view = new StudentView();
+
+        // Create Controller
+        StudentController controller =
+                new StudentController(student, view);
+
+        // Display Initial Details
+        System.out.println("Initial Student Details:");
+        controller.updateView();
+
+        // Update Student Details
+        controller.setStudentName("Magnus Carlson");
+        controller.setStudentGrade("A+");
+
+        System.out.println("\nUpdated Student Details:");
+        controller.updateView();
+    }
+}
